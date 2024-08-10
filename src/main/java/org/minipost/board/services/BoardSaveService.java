4d490bb3af2f6459b22e01ba6204bec07ba408c8
sswum <l5450won@naver.com>
2024-08-11 03:22:04 +0900

    게시글 저장 기능 내용 코멘트 달기 [이해하기]

--- src/main/java/org/minipost/board/services/BoardSaveService.java
+++ src/main/java/org/minipost/board/services/BoardSaveService.java
@@ -12,18 +12,18 @@ import java.util.Objects;
 @Service
 public class BoardSaveService {
 
-    private final BoardRepository boardRepository;
+    private final BoardRepository boardRepository; //저장소를 사용하기 위해 final
 
 
-    public void save(RequestPost form) {
-        Long seq = Objects.requireNonNullElse(form.getSeq(), 0L);
-        Board board = boardRepository.findById(seq).orElseGet(Board::new);
+    public void save(RequestPost form) { // 커맨드객체를 매개변수로 사용
+        Long seq = Objects.requireNonNullElse(form.getSeq(), 0L); //커맨드객체의 seq를 먼저 조회
+        Board board = boardRepository.findById(seq).orElseGet(Board::new); // 게시판 레포지토리에서 seq 조회 아니면 새롭게 게시판 생성
 
-        board.setSeq(form.getSeq());
-        board.setTitle(form.getTitle());
-        board.setName(form.getName());
-        board.setContent(form.getContent());
+        board.setSeq(form.getSeq()); // form 들어간 seq 값 변경
+        board.setTitle(form.getTitle()); // title값 변경
+        board.setName(form.getName()); // ...
+        board.setContent(form.getContent()); // ...
 
-        boardRepository.saveAndFlush(board);
+        boardRepository.saveAndFlush(board); // 게시판 레포지토리에 저장
     }
 }
